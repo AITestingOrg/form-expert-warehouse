282fb2f33a1cf371939b1fb21a998c91ada28c8e
Justin Phillips <euangelion3o9@gmail.com>
2018-05-25 13:58:28 -0400

    Initial codebase

---
+++ src/main/kotlin/org/aist/aide/formexpertwarehouse/FormExpertWarehouseApplication.kt
@@ -0,0 +1,11 @@
+package org.aist.aide.formexpertwarehouse
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FormExpertWarehouseApplication
+
+fun main(args: Array<String>) {
+    runApplication<FormExpertWarehouseApplication>(*args)
+}
