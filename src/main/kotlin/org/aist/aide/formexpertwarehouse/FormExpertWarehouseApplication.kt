feb6e3590a5e72e850e16811499ee534f96086a4
Justin Phillips <euangelion3o9@gmail.com>
2018-05-31 14:45:13 -0400

    Added histrix and fixed indentation in build.gradle

--- src/main/kotlin/org/aist/aide/formexpertwarehouse/FormExpertWarehouseApplication.kt
+++ src/main/kotlin/org/aist/aide/formexpertwarehouse/FormExpertWarehouseApplication.kt
@@ -2,9 +2,11 @@ package org.aist.aide.formexpertwarehouse
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 class FormExpertWarehouseApplication
 
